93b4d894b0d40d2114a849046f0ffc4a536932a0
Turtel216 <papadimitrios216@gmail.com>
2024-08-30 23:33:32 +0300

    Added command line arg handler

--- interpreter/src/main.rs
+++ interpreter/src/main.rs
@@ -1,5 +1,54 @@
 mod lexer;
 
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() == 1 {
+        repl();
+    } else if args.len() == 2 {
+        match run_file(&args[1]) {
+            Ok(()) => println!("Succes"),
+            Err(e) => println!("Error: {e:?}"),
+        }
+    } else {
+        println!("Usage: pf [path]");
+    }
+}
+
+// Command line interpreter
+fn repl() -> () {
+    use std::io::{stdin, stdout, Write};
+    let mut str = String::new();
+
+    loop {
+        print!("> ");
+        let _ = stdout().flush();
+        stdin()
+            .read_line(&mut str)
+            .expect("Did not enter a corrent string.");
+
+        if let Some('\n') = str.chars().next_back() {
+            str.pop();
+        }
+        if let Some('\r') = str.chars().next_back() {
+            str.pop();
+        }
+
+        println!("You typed: {}", str);
+    }
+}
+
+// File interpreter
+fn run_file(_path: &String) -> std::io::Result<()> {
+    let file = File::open(_path)?;
+    let mut buf_reader = BufReader::new(file);
+    let mut contents = String::new();
+    buf_reader.read_to_string(&mut contents)?;
+    println!("{contents}");
+    Ok(())
 }
