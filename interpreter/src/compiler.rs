d0876b943972d4d21d12991f286670554aa87509
Turtel216 <papadimitrios216@gmail.com>
2024-09-03 20:38:51 +0300

    Added byte code emition to compiler

--- interpreter/src/compiler.rs
+++ interpreter/src/compiler.rs
@@ -3,35 +3,43 @@
 // license that can be found in the LICENSE file
 
 use crate::{
-    chunk::Chunk,
+    chunk::{Chunk, OpCode},
     lexer::{Token, TokenType},
 };
 
 pub struct Compiler<'c> {
     current: usize,
     tokens: &'c Vec<Token>,
+    chunk: Chunk,
     had_error: bool,
     panic_mode: bool,
 }
 
 impl<'c> Compiler<'c> {
-    pub fn new(tokens: &'c Vec<Token>) -> Self {
+    pub fn new(tokens: &'c Vec<Token>, chunk: Chunk) -> Self {
         return Compiler {
             tokens,
             current: 0,
             had_error: false,
             panic_mode: false,
+            chunk,
         };
     }
     pub fn compile(&mut self, source: &'c String, chunk: Chunk) -> bool {
         self.had_error = false;
         self.panic_mode = false;
+        self.chunk = chunk;
 
         self.advance();
         self.expression();
         self.consume(TokenType::TokenEOF, "Expected end of expression.");
 
+        self.end_compiler();
         return !self.had_error;
+    }
+
+    fn end_compiler(&mut self) -> () {
+        self.emit_return();
     }
 
     fn advance(&mut self) -> () {
@@ -54,6 +62,20 @@ impl<'c> Compiler<'c> {
         }
 
         self.error_at_current(msg);
+    }
+
+    fn emit_byte(&mut self, byte: OpCode) -> () {
+        self.chunk
+            .write_chunk(byte, self.tokens[self.current - 1].line);
+    }
+
+    fn emit_bytes(&mut self, byte1: OpCode, byte2: OpCode) -> () {
+        self.emit_byte(byte1);
+        self.emit_byte(byte2);
+    }
+
+    fn emit_return(&mut self) -> () {
+        self.emit_byte(OpCode::OpReturn);
     }
 
     fn error_at_current(&mut self, msg: &'c str) -> () {
